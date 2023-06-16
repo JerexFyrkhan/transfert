9d28b6826720732ce143fbccb480c281b8411d5a
Julien Waguet <julien.waguet@assoconnect.com>
2023-06-16 15:30:24 +0200

    Question13

--- src/Controller/ManageObjectController.php
+++ src/Controller/ManageObjectController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\Object756Repository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +15,13 @@ class ManageObjectController extends AbstractController
 
     private Object756Repository $object756Repository;
     private EntityManagerInterface $entityManager;
+    private UserRepository $userRepository;
 
-    public function __construct(Object756Repository $object756Repository, EntityManagerInterface $entityManager)
+    public function __construct(Object756Repository $object756Repository, UserRepository $userRepository, EntityManagerInterface $entityManager)
     {
         $this->object756Repository = $object756Repository;
         $this->entityManager = $entityManager;
+        $this->userRepository = $userRepository;
     }
 
     #[Route('/manage/object/{id}', name: 'app_manage_object')]
@@ -31,8 +34,10 @@ class ManageObjectController extends AbstractController
 
         if (null !== $request->get('action')) {
             if ('update' === $request->get('action')) {
+                $proprietaire = $this->userRepository->find($request->get('proprietaire'));
                 $object->setNom($request->get('nomNew'));
                 $object->setDescription($request->get('description'));
+                $object->setProprietaire($proprietaire);
                 $this->object756Repository->save($object, true);
             }
             if ('delete' === $request->get('action')) {
@@ -40,8 +45,10 @@ class ManageObjectController extends AbstractController
             }
         }
 
+        $users = $this->userRepository->findAll();
         return $this->render('manage_object/index.html.twig', [
             'object' => $object,
+            'users' => $users,
         ]);
     }
 }
